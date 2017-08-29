fd90e925bab45871aced84ed711b46a24d1fc5be
Igor vIEIRA <igor.p.r.vieira@gmail.com>
2017-08-29 14:16:47 -0300

    change path for assets

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,30 @@ const pump = require('pump');
 
 
 gulp.task('image-optimize', () =>
-    gulp.src('./images/*')
+    gulp.src('./assets/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'))
 );
 
 
 gulp.task('sass', () => {
- 	return gulp.src('./sass/**/*.scss')
+ 	return gulp.src('./assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 
 });
 
 gulp.task('sass-compress', () => {
- return gulp.src('./sass/**/*.scss')
+ return gulp.src('./assets/sass/**/*.scss')
    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
    .pipe(gulp.dest('./dist/css/minify'));
 });
 
 gulp.task('watch', () => {
 //    gulp.watch(['./assets/js/**/*.js'],['js-compress']); 
-    gulp.watch(['./images/*.*'],['image-optimize']);
-	 gulp.watch(['./sass/**/*.scss'], ['sass']);
-      gulp.watch(['./sass/**/*.scss'], ['sass-compress']);
+    gulp.watch(['./assets/images/*.*'],['image-optimize']);
+	 gulp.watch(['./assets/sass/**/*.scss'], ['sass']);
+      gulp.watch(['./assets/sass/**/*.scss'], ['sass-compress']);
 })
 
 
