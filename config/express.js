7d9c4267c39e8eebcae9e22412b37d1decca93d4
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-08-29 23:23:56 -0300

    setup for heroku completed

---
+++ config/express.js
@@ -0,0 +1,11 @@
+const express =  require('express')
+
+module.exports = function(){
+
+	const app = express();
+	app.set('port',(process.env.PORT || 3000))
+	app.use(express.static('./public'));
+
+	return app;
+
+}
---
+++ server.js
@@ -0,0 +1,8 @@
+const http  =  require('http')
+const app  = require('./config/express.js')();
+const logger = require('logger')
+
+http.createServer(app)
+		.listen(app.get('port'), () => {
+		console.log(`Server is running: ${app.get('port')}`);
+});
