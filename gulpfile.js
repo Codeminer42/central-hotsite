1f04643d3f8fe5eea55487a6eaaf1c7f31e4543e
moiseshilario <moiseshilariu@gmail.com>
2017-08-31 16:01:31 -0300

    Adding js compress to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-
-
+gulp.task('js-compress', (cb) => {
+  pump([
+        gulp.src('./assets/js/*.js'),
+        uglify(),
+        gulp.dest('./public/dist/')
+    ],
+    cb
+  );
+});
 
 gulp.task('image-optimize', () =>
   gulp.src('./public/assets/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./public/dist/images'))
 );
-
 
 gulp.task('sass', () => {
   return gulp.src('./public/assets/sass/**/*.scss')
@@ -26,13 +33,13 @@ gulp.task('sass-compress', () => {
 });
 
 gulp.task('watch', () => {
-  //    gulp.watch(['./public/assets/js/**/*.js'],['js-compress']); 
+  gulp.watch(['./public/assets/js/**/*.js'],['js-compress']); 
   gulp.watch(['./public/assets/images/*.*'], ['image-optimize']);
   gulp.watch(['./public/assets/sass/**/*.scss'], ['sass']);
   gulp.watch(['./public/assets/sass/**/*.scss'], ['sass-compress']);
 })
 
-
 gulp.task('default', () => {
   gulp.start('image-optimize', 'sass', 'sass-compress');
 });
+
